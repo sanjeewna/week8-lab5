30d63da4db1083ea3479d932b4280c9e863ab51c
sanjeewna <sanjeewnadevi93@gmail.com>
2023-12-06 05:58:26 +0200

    week8-lab5 completed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from 'react';
+import Signup from './Signup';
+import GenericForm from './GenericForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Forms Lab</h1>
+      <Signup />
+      <h2>Generic Form</h2>
+      <GenericForm />
+    </div>
+  );
+}
+
+export default App;
